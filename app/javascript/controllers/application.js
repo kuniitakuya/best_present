f1f52bb9124a451b342a6aa3aa45d85028719aa4
國井拓也 <knight92137@gmail.com>
2022-12-13 02:19:46 +0900

    Add: TopページにVueを反映して表示

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,15 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+import * as Vue from "vue";
+
+const App = Vue.createApp({
+    data(){
+        return {
+            text: 'Hello Vue'
+        }
+    },
+})
+
+App.mount("#app");
