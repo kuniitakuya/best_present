0aab45d77b8df214181339faefc89d22a98e95ed
國井拓也 <knight92137@gmail.com>
2023-01-11 16:38:58 +0900

    Add: ジェネレーターの設定を追加

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,11 @@ module BestPresent
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    #rails gコマンドで生成するファイルの設定
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
