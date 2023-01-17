72ebc3fd52b2be7482ac043ff8c4857729704d59
國井拓也 <knight92137@gmail.com>
2023-01-17 19:13:09 +0900

    Remove: デプロイエラーのためVue一時廃止

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,9 +5,3 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-
-if Rails.env.development?
-  pin "vue", to: "vue--dist--vue.esm-browser.js.js" # @3.2.45
-else
-  pin "vue", to: "vue--dist--vue.esm-browser.prod.js.js" # @3.2.45
-end
