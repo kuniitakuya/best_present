011308a3c32ab0a452d1ab9cc2822398e0dd20ba
國井拓也 <knight92137@gmail.com>
2023-01-27 20:46:49 +0900

    Add: 欲しいものリスト作成のrouteを追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new create]
   resource :mypage, only: %i[show edit update]
-  resources :wish_lists, only: %i[index]
+  resources :wish_lists, only: %i[index new create]
 end
