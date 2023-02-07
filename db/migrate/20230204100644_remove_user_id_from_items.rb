d95400244fc1afceb60fd8c4e0c26f8ca062d315
國井拓也 <knight92137@gmail.com>
2023-02-07 14:51:33 +0900

    Remove: 必要ないマイグレーションファイル削除

--- db/migrate/20230204100644_remove_user_id_from_items.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUserIdFromItems < ActiveRecord::Migration[7.0]
-  def change
-    remove_foreign_key :items, :users
-    remove_reference :items, :user, index: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_04_183053) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_27_122631) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,9 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_04_183053) do
     t.integer "price", null: false
     t.string "image"
     t.string "url", null: false
+    t.bigint "wish_list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "wish_list_id"
     t.index ["wish_list_id"], name: "index_items_on_wish_list_id"
   end
 
