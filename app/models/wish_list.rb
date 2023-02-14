1cc2dba900bac44889e5a4be5cdd1451bb08ab8c
國井拓也 <knight92137@gmail.com>
2023-02-14 21:35:27 +0900

    Add: 検索対象のカラムをモデルに設定

--- app/models/wish_list.rb
+++ app/models/wish_list.rb
@@ -2,5 +2,9 @@ class WishList < ApplicationRecord
   belongs_to :user
   has_many :items, dependent: :destroy
 
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "list_name", "updated_at", "user_id"]
+  end
+
   validates :list_name, presence: true, length: { maximum: 255 }
 end
