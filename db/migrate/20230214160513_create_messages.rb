6949fc0ee047b9fe713761c5ec0a2955cf9135a3
國井拓也 <knight92137@gmail.com>
2023-02-19 15:15:19 +0900

    Fix: messageテーブルにカラム追加

--- db/migrate/20230214160513_create_messages.rb
+++ db/migrate/20230214160513_create_messages.rb
@@ -3,8 +3,9 @@ class CreateMessages < ActiveRecord::Migration[7.0]
     create_table :messages do |t|
       t.text :text, null: false
       t.string :message_image, null: false
-      t.references :user, null: false, foreign_key: true
-      t.references :wish_list, null: false, foreign_key: true
+      t.string :select_item, null: false
+      t.references :user, foreign_key: true
+      t.references :wish_list, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -31,8 +31,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_160513) do
   create_table "messages", force: :cascade do |t|
     t.text "text", null: false
     t.string "message_image", null: false
-    t.bigint "user_id", null: false
-    t.bigint "wish_list_id", null: false
+    t.string "select_item", null: false
+    t.bigint "user_id"
+    t.bigint "wish_list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_messages_on_user_id"
