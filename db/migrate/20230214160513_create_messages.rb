98e37843f0b671e57bb0f8b81378190d55c8c28b
國井拓也 <knight92137@gmail.com>
2023-02-27 23:30:46 +0900

    Fix: カラム修正

--- db/migrate/20230214160513_create_messages.rb
+++ db/migrate/20230214160513_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
       t.text :text, null: false
-      t.string :message_image, null: false
+      t.string :message_image
       t.string :select_item, null: false
       t.references :user, foreign_key: true
       t.references :wish_list, foreign_key: true
