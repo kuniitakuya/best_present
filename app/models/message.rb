9ddeba5eeff4b2c813d991c083dbeb117de40ad9
國井拓也 <knight92137@gmail.com>
2023-02-28 18:52:09 +0900

    Fix: メッセージの文字制限変更

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :wish_list
 
-  validates :text, presence: true, length: { maximum: 255 }
+  validates :text, presence: true, length: { maximum: 90 }
   validates :select_item, presence: true
 end
