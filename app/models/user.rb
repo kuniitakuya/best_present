7ebdc474ca4b903e3d4fbba737e269a116d9ad97
國井拓也 <knight92137@gmail.com>
2023-03-17 23:52:23 +0900

    Add: ransackモデル設定追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :messages, dependent: :destroy
 
+  def self.ransackable_attributes(_auth_object = nil)
+    ["id", "name", "email", "created_at" ]
+  end
+
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
