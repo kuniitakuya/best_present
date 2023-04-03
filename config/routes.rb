7254b84b8d3c5a9f7d0065c620eb0b140c446d5e
國井拓也 <knight92137@gmail.com>
2023-04-03 23:11:38 +0900

    Add: LetterOpenerWebにアクセスするために必要な記述を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   namespace :admin do
     root to: 'users#index'
     get 'login', to: 'user_sessions#new'
