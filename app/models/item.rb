f12a9c948984bb4e72c16cc135c596039c1204ac
國井拓也 <knight92137@gmail.com>
2023-04-15 18:32:35 +0900

    Remove: modelのメソッド削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,23 +6,4 @@ class Item < ApplicationRecord
   validates :price, presence: true
   validates :url, presence: true
   validates :save_list, presence: true
-  validate :validate_keyword, on: :search
-
-  private
-
-  def validate_keyword
-    return if keyword.blank?
-
-    if keyword.length > 128
-      errors.add(:keyword, 'は半角128文字以内で入力してください')
-    end
-
-    keyword.split.each do |word|
-      if word.length == 1 && word !~ /\p{Hiragana}|\p{Katakana}|[[:punct:]]/
-        errors.add(:keyword, '各検索キーワードは半角2文字 もしくは 全角1文字 以上で指定してください')
-      elsif word =~ /\p{Hiragana}|\p{Katakana}|[[:punct:]]/ && word.length < 2
-        errors.add(:keyword, 'ひらがな・カタカナ・記号を含む検索キーワードは2文字以上で指定してください')
-      end
-    end
-  end
 end
